676edc879bc24881a84b4391bf0bba4caf6b98d2
Abhijit Samantaray <Abhijit Samantaray@LAPTOP-IS27TH9F>
2019-10-17 01:31:05 +0530

    1st commit

---
+++ src/com/proj01/git/Gitnew.java
@@ -0,0 +1,11 @@
+package com.proj01.git;
+
+public class Gitnew {
+
+	public static void main(String[] args) {
+
+		System.out.println("Client project ,please be checked......................");
+		System.out.println("Gitnew.main()");
+	}
+
+}
